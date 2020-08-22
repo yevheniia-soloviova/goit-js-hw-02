4ed1eb9e742491bfde4be98dde07cb2db90c2ba7
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-08-22 18:33:57 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,28 @@
-const name = "«Генератор защитного поля»"; // Write code on this line
-let price = 1000;
-// Write code on this line
-const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+const getItemsString = function (array) {
+  "use strict";
+  // Write code under this line
+  let result = "";
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+  return result;
+};
 
-price = 2000;
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+/*
+'1 - Mango
+2 - Poly
+3 - Ajax
+4 - Lux
+5 - Jay
+6 - Kong
+'
+*/
 
-const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
-// Write code on this line
-console.log(firstMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
-console.log(secondMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
+console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
